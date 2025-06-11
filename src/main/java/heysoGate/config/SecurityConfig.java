07d75980dab4485fe39ed5b6b4a386543909be9a
soyoung <makemeha2@gmail.com>
2025-06-11 19:40:56 +0900

    CORS 설정

--- src/main/java/heysoGate/config/SecurityConfig.java
+++ src/main/java/heysoGate/config/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,6 +28,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // TODO : 추후 운영에서는 swagger가 안보이고 개발에서만 보이도록 분기처리
         http
+                .cors(cors -> {})
                 .securityMatcher("/**")
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement((sessionManagement) ->
@@ -45,5 +51,19 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        //config.setAllowedOrigins(List.of("http://localhost:5173")); // 필요시 * 로 허용
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // 필요한 경우만
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
